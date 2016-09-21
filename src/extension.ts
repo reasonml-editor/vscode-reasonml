68305b299db4201e10406d52e458621e64254bce
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-09-20 18:17:02 -0600

    Add more language config settings for indent, etc.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,37 @@ import * as vscode from 'vscode';
 import * as client from './client';
 
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.setLanguageConfiguration('reason', {
+      indentationRules: {
+        decreaseIndentPattern: /^(.*\*\/)?\s*\}.*$/,
+        increaseIndentPattern: /^.*\{[^}"']*$/,
+      },
+      onEnterRules: [
+        {
+          beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+          afterText: /^\s*\*\/$/,
+          action: { indentAction: vscode.IndentAction.IndentOutdent, appendText: ' * ' }
+        },
+        {
+          beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+          action: { indentAction: vscode.IndentAction.None, appendText: ' * ' }
+        },
+        {
+          beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
+          action: { indentAction: vscode.IndentAction.None, appendText: '* ' }
+        },
+        {
+          beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
+          action: { indentAction: vscode.IndentAction.None, removeText: 1 }
+        },
+        {
+          beforeText: /^(\t|(\ \ ))*\ \*[^/]*\*\/\s*$/,
+          action: { indentAction: vscode.IndentAction.None, removeText: 1 }
+        }
+      ],
+      wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
+    }));
   context.subscriptions.push(client.launch(context));
 }
 
