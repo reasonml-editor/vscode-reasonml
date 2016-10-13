f0df93d5d69c82c719a4dd05bafafeff1120b8d0
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-10-13 09:33:03 -0600

    Compute diagnostics for all files on open or save

--- src/server/session.ts
+++ src/server/session.ts
@@ -1,24 +1,64 @@
 import * as merlin from "../shared/merlin";
+import * as method from "./method";
 import * as types from "../shared/types";
 import * as processes from "./processes";
 import * as _ from "lodash";
 import * as server from "vscode-languageserver";
 
-export class Synchronizer {
+export class Diagnostics {
   private session: Session;
+
   constructor(session: Session) {
     this.session = session;
     return this;
   }
+
+  clear(event: types.TextDocumentIdentifier): void {
+    const diagnostics = [];
+    const uri = event.uri;
+    this.session.connection.sendDiagnostics({ diagnostics, uri });
+  }
+
+  refreshDebounced = _.debounce(async (event: types.TextDocumentIdentifier) => {
+    const response = await this.session.merlin.query(merlin.Query.errors(), event.uri);
+    if (response.class !== "return") return;
+    const diagnostics = response.value.map(merlin.ErrorReport.intoCode);
+    this.session.connection.sendDiagnostics({ diagnostics, uri: event.uri });
+  }, 500, { trailing: true });
+
+  async refresh(event: types.TextDocumentIdentifier): Promise<void> {
+    const document = await method.getTextDocument(this.session, event);
+    const request = merlin.Sync.tell("start", "end", document.content);
+    await this.session.merlin.sync(request, event.uri);
+    const response = await this.session.merlin.query(merlin.Query.errors(), event.uri);
+    if (response.class !== "return") return;
+    const diagnostics = response.value.map(merlin.ErrorReport.intoCode);
+    this.session.connection.sendDiagnostics({ diagnostics, uri: event.uri });
+  }
+
+  async refreshWorkspace(event: types.TextDocumentIdentifier): Promise<void> {
+    const workspaceMods = await method.getModules(this.session, event);
+    for (const uri of workspaceMods) this.refresh(uri);
+  }
+}
+
+export class Synchronizer {
+  private session: Session;
+
+  constructor(session: Session) {
+    this.session = session;
+    return this;
+  }
+
   listen() {
     this.session.connection.onDidCloseTextDocument((event) => {
-      this.session.diagnosticsClear(event.textDocument);
+      this.session.diagnostics.clear(event.textDocument);
     });
 
     this.session.connection.onDidOpenTextDocument(async (event): Promise<void> => {
       const request = merlin.Sync.tell("start", "end", event.textDocument.text);
       await this.session.merlin.sync(request, event.textDocument.uri);
-      this.session.diagnosticsRefresh(event.textDocument);
+      this.session.diagnostics.refreshWorkspace(event.textDocument);
     });
 
     this.session.connection.onDidChangeTextDocument(async (event): Promise<void> => {
@@ -30,7 +70,11 @@ export class Synchronizer {
           await this.session.merlin.sync(request, event.textDocument.uri);
         }
       }
-      this.session.diagnosticsRefresh(event.textDocument);
+      this.session.diagnostics.refreshDebounced(event.textDocument);
+    });
+
+    this.session.connection.onDidSaveTextDocument(async (event): Promise<void> => {
+      this.session.diagnostics.refreshWorkspace(event.textDocument);
     });
   }
 }
@@ -40,25 +84,14 @@ export class Session {
     new server.IPCMessageReader(process),
     new server.IPCMessageWriter(process),
   );
+  readonly diagnostics: Diagnostics;
   readonly merlin = new processes.Merlin();
   readonly synchronizer: Synchronizer;
 
   constructor() {
+    this.diagnostics = new Diagnostics(this);
     this.synchronizer = new Synchronizer(this);
     return this;
-  }
-
-  diagnosticsRefresh = _.debounce(async (event: types.TextDocumentIdentifier): Promise<void> => { // tslint:disable-line
-    const errorResponse = await this.merlin.query(merlin.Query.errors(), event.uri);
-    if (errorResponse.class !== "return") return;
-    const diagnostics = errorResponse.value.map(merlin.ErrorReport.intoCode);
-    this.connection.sendDiagnostics({ diagnostics, uri: event.uri });
-  }, 500, { trailing: true });
-
-  diagnosticsClear(event: types.TextDocumentIdentifier): void {
-    const diagnostics = [];
-    const uri = event.uri;
-    this.connection.sendDiagnostics({ diagnostics, uri });
   }
 
   listen() {
