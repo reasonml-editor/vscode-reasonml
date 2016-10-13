c01e16db556d000f4c40df7d5f896ee5f7f2bcbf
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-10-13 10:06:54 -0600

    Cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -3,86 +3,87 @@
 import * as client from "./client";
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.languages.setLanguageConfiguration("reason", {
-      indentationRules: {
-        decreaseIndentPattern: /^(.*\*\/)?\s*\}.*$/,
-        increaseIndentPattern: /^.*\{[^}"']*$/,
+const reasonConfiguration = {
+  indentationRules: {
+    decreaseIndentPattern: /^(.*\*\/)?\s*\}.*$/,
+    increaseIndentPattern: /^.*\{[^}"']*$/,
+  },
+  onEnterRules: [
+    {
+      beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+      afterText: /^\s*\*\/$/,
+      action: {
+        indentAction: vscode.IndentAction.IndentOutdent,
+        appendText: " * ",
       },
-      onEnterRules: [
-        {
-          beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-          afterText: /^\s*\*\/$/,
-          action: {
-            indentAction: vscode.IndentAction.IndentOutdent,
-            appendText: " * ",
-          },
-        },
-        {
-          beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
-          action: {
-            indentAction: vscode.IndentAction.None,
-            appendText: " * ",
-          },
-        },
-        {
-          beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
-          action: {
-            indentAction: vscode.IndentAction.None,
-            appendText: "* ",
-          },
-        },
-        {
-          beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
-          action: {
-            indentAction: vscode.IndentAction.None,
-            removeText: 1,
-          },
-        },
-        {
-          beforeText: /^(\t|(\ \ ))*\ \*[^/]*\*\/\s*$/,
-          action: {
-            indentAction: vscode.IndentAction.None,
-            removeText: 1,
-          },
-        },
-        {
-          beforeText: /^.*\bfun\b\s*$/,
-          action: {
-            indentAction: vscode.IndentAction.None,
-            appendText: "  | ",
-          }
-        },
-        {
-          beforeText: /^.*\btype\b.*=.*$/,
-          action: {
-            indentAction: vscode.IndentAction.None,
-            appendText: "  | ",
-          }
-        },
-        {
-          beforeText: /^(\t|(\ \ ))*\|(.*[^;])?$/,
-          action: {
-            indentAction: vscode.IndentAction.None,
-            appendText: "| ",
-          },
-        },
-        {
-          beforeText: /^(\t|(\ \ ))*\|(.*[;])$/,
-          action: {
-            indentAction: vscode.IndentAction.Outdent,
-          },
-        },
-        {
-          beforeText: /^(\t|(\ \ ))*;\s*$/,
-          action: {
-            indentAction: vscode.IndentAction.Outdent,
-          },
-        },
-      ],
-      wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
-    }));
+    },
+    {
+      beforeText: /^\s*\/\*\*(?!\/)([^\*]|\*(?!\/))*$/,
+      action: {
+        indentAction: vscode.IndentAction.None,
+        appendText: " * ",
+      },
+    },
+    {
+      beforeText: /^(\t|(\ \ ))*\ \*(\ ([^\*]|\*(?!\/))*)?$/,
+      action: {
+        indentAction: vscode.IndentAction.None,
+        appendText: "* ",
+      },
+    },
+    {
+      beforeText: /^(\t|(\ \ ))*\ \*\/\s*$/,
+      action: {
+        indentAction: vscode.IndentAction.None,
+        removeText: 1,
+      },
+    },
+    {
+      beforeText: /^(\t|(\ \ ))*\ \*[^/]*\*\/\s*$/,
+      action: {
+        indentAction: vscode.IndentAction.None,
+        removeText: 1,
+      },
+    },
+    {
+      beforeText: /^.*\bfun\b\s*$/,
+      action: {
+        indentAction: vscode.IndentAction.None,
+        appendText: "  | ",
+      }
+    },
+    {
+      beforeText: /^.*\btype\b.*=.*$/,
+      action: {
+        indentAction: vscode.IndentAction.None,
+        appendText: "  | ",
+      }
+    },
+    {
+      beforeText: /^(\t|(\ \ ))*\|(.*[^;])?$/,
+      action: {
+        indentAction: vscode.IndentAction.None,
+        appendText: "| ",
+      },
+    },
+    {
+      beforeText: /^(\t|(\ \ ))*\|(.*[;])$/,
+      action: {
+        indentAction: vscode.IndentAction.Outdent,
+      },
+    },
+    {
+      beforeText: /^(\t|(\ \ ))*;\s*$/,
+      action: {
+        indentAction: vscode.IndentAction.Outdent,
+      },
+    },
+  ],
+  wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
+};
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(vscode.languages.setLanguageConfiguration("reason", reasonConfiguration));
   context.subscriptions.push(client.launch(context));
 }
 
