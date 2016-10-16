a34d3f76971aa5025018754e0bde8d1c2dc4dbcd
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-10-16 14:31:45 -0600

    Add more ignore patterns to ocamldoc parser

--- src/shared/parser/ocamldoc/index.ts
+++ src/shared/parser/ocamldoc/index.ts
@@ -1,9 +1,11 @@
 const parser = require("./grammar"); // tslint:disable-line
 
 export const ignore = new RegExp([
+  /^Needed cmti file of module/,
   /^No documentation available/,
   /^Not a valid identifier/,
   /^Not in environment '.*'/,
+  /^didn't manage to find/,
 ].map((rx) => rx.source).join("|"));
 
 export function intoMarkdown(ocamldoc: string): string {
