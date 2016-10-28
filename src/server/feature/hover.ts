1ee7cac4cf32eef13d85dfb00f9d8e72d32218e7
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-10-28 13:41:41 -0600

    Use word token to select appropriate hover grammar
    This avoids the problem of having to try and resolve ambiguity between
    types and signatures in the highlighting rules which is messy and
    perhaps even impossible to do accurately in general.

--- src/server/feature/hover.ts
+++ src/server/feature/hover.ts
@@ -5,13 +5,16 @@ import * as server from "vscode-languageserver";
 
 export default function(session: Session): server.RequestHandler<server.TextDocumentPositionParams, types.Hover, void> {
   return async (event, token) => {
+    const position = { position: event.position, uri: event.textDocument.uri };
+    const word = await command.getWordAtPosition(session, position);
     const markedStrings: types.MarkedString[] = [];
     const itemType = await command.getType(session, event);
     if (token.isCancellationRequested) return { contents: [] };
     const itemDocs = await command.getDocumentation(session, event);
     if (token.isCancellationRequested) return { contents: [] };
     if (itemType != null) {
-      markedStrings.push({ language: "reason.hover.type", value: itemType.type });
+      const language = /^[A-Z]/.test(word) ? "reason.hover.signature" : "reason.hover.type";
+      markedStrings.push({ language, value: itemType.type });
       markedStrings.push(merlin.TailPosition.intoCode(itemType.tail)); // FIXME: make configurable
       if (itemDocs != null && !parser.ocamldoc.ignore.test(itemDocs)) markedStrings.push(parser.ocamldoc.intoMarkdown(itemDocs));
     }
