51a460ddeb5f95b9953b2874bcb2c1aee36df2bb
Darin Morrison <freebroccolo@users.noreply.github.com>
2017-02-21 14:25:05 -0700

    Reimplement format on save to use onWillSave API

--- src/client/lifecycle/index.ts
+++ src/client/lifecycle/index.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 import * as client from "vscode-languageclient";
-import * as onDidSaveTextDocument from "./onDidSaveTextDocument";
+import * as onWillSaveTextDocument from "./onWillSaveTextDocument";
 
 export function registerAll(context: vscode.ExtensionContext, languageClient: client.LanguageClient): void {
-  onDidSaveTextDocument.register(context, languageClient);
+  onWillSaveTextDocument.register(context, languageClient);
 }
---
+++ src/client/lifecycle/onWillSaveTextDocument.ts
@@ -0,0 +1,25 @@
+import * as vscode from "vscode";
+import * as client from "vscode-languageclient";
+import { remote } from "../../shared";
+
+async function execute(languageClient: client.LanguageClient, event: vscode.TextDocumentWillSaveEvent): Promise<vscode.TextEdit[]> {
+  const textDocument = {
+    content: event.document.getText(),
+    languageId: event.document.languageId,
+    uri: event.document.uri.toString(),
+    version: event.document.version,
+  };
+  const response = await languageClient.sendRequest(remote.server.giveFormatted, textDocument);
+  if (response == null) return [];
+  const fullRange = new vscode.Range(
+    event.document.positionAt(0),
+    event.document.positionAt(textDocument.content.length - 1));
+  return [vscode.TextEdit.replace(fullRange, response)];
+}
+
+export function register(context: vscode.ExtensionContext, languageClient: client.LanguageClient): void {
+  context.subscriptions.push(
+    vscode.workspace.onWillSaveTextDocument((event) => {
+      if (event.document.languageId === "reason") event.waitUntil(execute(languageClient, event));
+    }));
+}
