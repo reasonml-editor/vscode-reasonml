ac55b3aa2abdc17cc7a2b65c8250389ab3d9ba60
Darin Morrison <freebroccolo@users.noreply.github.com>
2017-11-17 22:29:11 -0700

    Remove unused code

--- src/client/request/index.ts
+++ src/client/request/index.ts
@@ -1,16 +1,9 @@
 import * as vscode from "vscode";
 import * as client from "vscode-languageclient";
-import * as givePrefix from "./givePrefix";
-import * as giveText from "./giveText";
-import * as giveTextDocument from "./giveTextDocument";
-import * as giveWordAtPosition from "./giveWordAtPosition";
 
 export function registerAll(
-  context: vscode.ExtensionContext,
-  languageClient: client.LanguageClient,
+  _context: vscode.ExtensionContext,
+  _languageClient: client.LanguageClient,
 ): void {
-  givePrefix.register(context, languageClient);
-  giveText.register(context, languageClient);
-  giveTextDocument.register(context, languageClient);
-  giveWordAtPosition.register(context, languageClient);
+  return;
 }
