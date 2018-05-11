87b47c41c53c39642d55d7d1ab4f24bc0e8f5024
RaisCui <vdcoolzi@gmail.com>
2018-05-11 15:20:04 +0800

    for me build

--- src/client/command/fixEqualsShouldBeArrow.ts
+++ src/client/command/fixEqualsShouldBeArrow.ts
@@ -6,7 +6,15 @@ export function register(context: vscode.ExtensionContext, languageClient: clien
   context.subscriptions.push(
     vscode.commands.registerTextEditorCommand(
       "reason.codeAction.fixEqualsShouldBeArrow",
-      async (editor: vscode.TextEditor, _: any, [{ range: { end: position } }]: [LSP.Location]): Promise<void> => {
+      async (
+        editor: vscode.TextEditor,
+        _: any,
+        [
+          {
+            range: { end: position },
+          },
+        ]: [LSP.Location],
+      ): Promise<void> => {
         await editor.edit(editBuilder => {
           const editPosition = languageClient.protocol2CodeConverter.asPosition(position);
           editBuilder.insert(editPosition, ">");
