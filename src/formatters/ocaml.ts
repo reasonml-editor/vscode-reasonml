8f44e4ed6f8458f40b96140fd0212a4b5d662a3c
Rusty Key <iam@stfoo.ru>
2019-03-29 16:08:15 +0300

    use platform’s tmp dir

--- src/formatters/ocaml.ts
+++ src/formatters/ocaml.ts
@@ -1,5 +1,7 @@
 import { execSync } from "child_process";
 import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
 import { v4 as uuidv4 } from "uuid";
 import * as vscode from "vscode";
 import { getFormatter, getFullTextRange } from "./utils";
@@ -18,7 +20,7 @@ export function register() {
         if (!formatter) return [];
 
         if (textEditor) {
-          const tempFileName = `/tmp/vscode-reasonml-${uuidv4()}.ml`;
+          const tempFileName = path.join(os.tmpdir(), `vscode-reasonml-ocamlformat-${uuidv4()}.ml`);
           fs.writeFileSync(tempFileName, textEditor.document.getText(), "utf8");
           try {
             const filePath = textEditor.document.fileName;
--- src/formatters/reason.ts
+++ src/formatters/reason.ts
@@ -1,5 +1,7 @@
 import { execSync } from "child_process";
 import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
 import { v4 as uuidv4 } from "uuid";
 import * as vscode from "vscode";
 import { getFormatter, getFullTextRange } from "./utils";
@@ -17,7 +19,7 @@ export function register() {
         if (!formatter) return [];
 
         if (textEditor) {
-          const tempFileName = `/tmp/vscode-refmt-${uuidv4()}.re`;
+          const tempFileName = path.join(os.tmpdir(), `vscode-reasonml-refmt-${uuidv4()}.ml`);
           fs.writeFileSync(tempFileName, textEditor.document.getText(), "utf8");
           try {
             const formattedText = execSync(`${formatter} ${tempFileName}`).toString();
